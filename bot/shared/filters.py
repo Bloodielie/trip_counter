6ff1bdda9d8e96c774aa653f3282e3bcb567b4d8
Bloodielie <riopro2812@gmail.com>
2021-12-01 19:12:26 +0300

    fix: if role filter result False, other handlers don't work

--- bot/shared/filters.py
+++ bot/shared/filters.py
@@ -1,8 +1,8 @@
-from typing import Sequence, Set
+from typing import Sequence, Set, Union
 
 from aiogram import types
 from aiogram.dispatcher.filters import Filter
-from aiogram.dispatcher.handler import ctx_data
+from aiogram.dispatcher.handler import ctx_data, CancelHandler
 
 from bot.shared import text
 from bot.user.models import Role
@@ -17,21 +17,21 @@ def check_roles(roles: Sequence[Role], codenames: Set[str]) -> bool:
 
 
 class RoleFilter(Filter):
-    def __init__(self, roles: Sequence[str]):
-        self._roles = set(roles)
+    def __init__(self, roles: Union[str, Sequence[str]]):
+        self._roles = set(roles) if isinstance(roles, Sequence) else {roles}
 
     async def check(self, msg: types.Message) -> bool:
         data = ctx_data.get()
         user = data.get("user")
         if user is None:
             await msg.answer(text.PERMISSION_ERROR)
-            return False
+            raise CancelHandler
 
         if "*" in self._roles:
             return True
 
         if not check_roles(user.roles, self._roles):
             await msg.answer(text.PERMISSION_ERROR)
-            return False
+            raise CancelHandler
 
         return True
